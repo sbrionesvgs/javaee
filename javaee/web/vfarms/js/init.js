8f353dbab7d14a8d25d03091c45fae9799c13f00
sbriones <sbriones@vertere-gs.com>
2018-10-31 14:44:39 +0800

    push static web pages

---
+++ javaee/web/vfarms/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.tooltipped').tooltip();
+    $('.tabs').tabs();
+    $(".dropdown-trigger").dropdown();
+    $('.datepicker').datepicker();
+    $('select').formSelect();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
---
+++ javaee/web/vfarmwoutnode-modules/src/globaljs/init.js
@@ -0,0 +1,10 @@
+(function($){
+    $(function(){
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+      $('.tooltipped').tooltip();
+      $('.tabs').tabs();
+      $(".dropdown-trigger").dropdown();
+      $('.datepicker').datepicker();
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
